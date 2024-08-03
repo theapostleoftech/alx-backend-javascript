88e5b7fcc32005b9c289a5a0ba6a9be59086091c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-08-03 02:24:04 +0100

    solutions

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,19 @@
+/// <reference path="Teacher.ts" />
+/// <reference path="Subject.ts" />
+
+namespace Subjects {
+    export class Java extends Subject {
+      getRequirements(): string {
+        return "Here is the list of requirements for Java";
+      }
+  
+      getAvailableTeacher(): string {
+        if (this.teacher && this.teacher.experienceTeachingJava !== undefined && this.teacher.experienceTeachingJava > 0) {
+          return `Available Teacher: ${this.teacher.firstName}`;
+        } else {
+          return "No available teacher";
+        }
+      }
+    }
+  }
+  
