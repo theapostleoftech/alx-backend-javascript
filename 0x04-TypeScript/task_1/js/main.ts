fa75ac8a3458c06ad66ca7439e1f06f9fef9c27c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-08-03 01:54:40 +0100

    solution to task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,3 +15,44 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+
+// Interface for the printTeacher function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Implementation of the printTeacher function
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Interface for the constructor of StudentClass
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Interface for the StudentClass
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// StudentClass implementation
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
